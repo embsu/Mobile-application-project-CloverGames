b9cd70540d87c22f86bd6cfe1e0ad76a6ddaaad2
SiniKykyri <sini.kykyri@gmail.com>
2024-03-28 16:31:21 +0200

    Collision detection works

--- screens/FlappybirdScreen.js
+++ screens/FlappybirdScreen.js
@@ -1,27 +1,30 @@
 import React from "react";
-import { Canvas, useImage, Image, Group, Text, matchFont, Circle} from "@shopify/react-native-skia";
-import { useWindowDimensions, Platform, Alert} from "react-native";
-import { 
-  useSharedValue, 
-  withTiming, 
-  Easing, 
-  withSequence, 
-  withRepeat, 
-  useFrameCallback, 
-  useDerivedValue, 
-  interpolate, 
-  Extrapolation, 
+import { Canvas, useImage, Image, Group, Text, matchFont, Circle, Rect, rect } from "@shopify/react-native-skia";
+import { useWindowDimensions, Platform, Alert } from "react-native";
+import {
+  useSharedValue,
+  withTiming,
+  Easing,
+  withSequence,
+  withRepeat,
+  useFrameCallback,
+  useDerivedValue,
+  interpolate,
+  Extrapolation,
   useAnimatedReaction,
   runOnJS,
   cancelAnimation
 } from "react-native-reanimated";
 import { useEffect, useState } from "react";
-import { GestureHandlerRootView, GestureDetector, Gesture} from "react-native-gesture-handler";
+import { GestureHandlerRootView, GestureDetector, Gesture } from "react-native-gesture-handler";
 
 // Lets add GRAVITY to the world
 //Here cause gravity does not change
 const GRAVITY = 900
 const JUMP_FORCE = -400
+
+const pipeWidth = 104
+const pipeHeight = 640
 
 const FlappybirdScreen = () => {
 
@@ -38,14 +41,40 @@ const FlappybirdScreen = () => {
   const gameOver = useSharedValue(false)
   const x = useSharedValue(width)
 
-  const birdY = useSharedValue(height/3)
+  const birdY = useSharedValue(height / 3)
   const birdYVelocity = useSharedValue(100)
   const birdPos = {
-    x: width / 4}
+    x: width / 4
+  }
 
   const birdCenterX = useDerivedValue(() => birdPos.x + 32)
-
   const birdCenterY = useDerivedValue(() => birdY.value + 24)
+  //Let's set the pipe offset. If offset is -100 pipe is upper and otherwise. Toppipe: offset - x, bottonpipe x + offset. 
+  //Thats cause we dont want to move the pipes same direction. 
+  const pipeOffset = 0
+
+  //Lets do multiple obstacles
+  const obstacles = useDerivedValue(() => {
+    const allObstacles = []
+    // add bottom pipe
+    allObstacles.push({
+      x: x.value,
+      y: height - 320 + pipeOffset,
+      h: pipeHeight,
+      w: pipeWidth
+    })
+
+    // add top pipe
+    allObstacles.push({
+      x: x.value,
+      y: pipeOffset - 320,
+      h: pipeHeight,
+      w: pipeWidth
+    })
+
+    return allObstacles
+
+  })
 
   //Background animation
   useEffect(() => {
@@ -57,50 +86,63 @@ const FlappybirdScreen = () => {
       withSequence(
         withTiming(-200, { duration: 3000, easing: Easing.linear }), // This is the animation for pipes moving from right to left
         withTiming(width, { duration: 0 }) // this returns the pipes to the right side of the screen, duration is 0 so it happens instantly
-      ), 
+      ),
       -1
     )
   }
 
   //Scoring system
   useAnimatedReaction(
-    () =>  x.value,
-    (currentValue, previousValue ) => {
+    () => x.value,
+    (currentValue, previousValue) => {
       const middle = birdPos.x
-      if(
+      if (
         currentValue !== previousValue &&
         previousValue &&
         currentValue <= middle &&
         previousValue > middle
-        ){
+      ) {
         //Do something
-        runOnJS(setScore)(score +1) //This is just cause we are using state. We need to use runOnJS to run the function
-        console.log('Score ++')
-
+        runOnJS(setScore)(score + 1) //This is just cause we are using state. We need to use runOnJS to run the function
+      }
     }
-  }
   )
+
+  const isPointCollingWithRect = (point, rect) => {
+    'worklet';
+    return (
+      point.x >= rect.x && // right of the left edge AND
+      point.x <= rect.x + rect.w && // left of the right edge AND
+      point.y >= rect.y && // below the top AND
+      point.y <= rect.y + rect.h // above the bottom
+    )
+  }
 
   // Collision detection
   useAnimatedReaction(
-  () => birdY.value, //This is the birdY value we are watching
-  (currentValue, previousValue ) => {
-    // Ground and sky collision detection (game over if bird hits the ground or the sky tohigh)
-    if(currentValue > height - 150 || currentValue < 0){
-      gameOver.value = true
-    }
+    () => birdY.value, //This is the birdY value we are watching
+    (currentValue, previousValue) => {
+      // Ground and sky collision detection (game over if bird hits the ground or the sky tohigh)
+      if (currentValue > height - 150 || currentValue < 0) {
+        gameOver.value = true
+      }
 
-    // Pipe collision detection
-    if(birdPos.x >= x.value){
-      gameOver.value = true
-    }
-  })
+      const isColliding = obstacles.value.some((rect) => 
+        isPointCollingWithRect(
+          { x: birdCenterX.value, y: birdCenterY.value }, 
+          rect)
+      )
+
+      if (isColliding) {
+        gameOver.value = true
+      }
+    })
 
   // Mikä tämän idea oikein olikaan? No selvitä maanantaina
-    useAnimatedReaction(
+  useAnimatedReaction(
     () => gameOver.value, // This is the value we are watching
-    (currentValue, previousValue ) => {
-      if(currentValue && !previousValue){
+    (currentValue, previousValue) => {
+      if (currentValue && !previousValue) {
         cancelAnimation(x) // Stops the animation
       }
     })
@@ -109,31 +151,32 @@ const FlappybirdScreen = () => {
   // dt is the time since the last frame
   // We use this to calculate the new position of the bird
   // dividing by 1000 to convert from milliseconds to seconds
-  useFrameCallback(({timeSincePreviousFrame: dt}) => {
-    if(!dt || gameOver.value){
-      return}
+  useFrameCallback(({ timeSincePreviousFrame: dt }) => {
+    if (!dt || gameOver.value) {
+      return
+    }
     birdY.value = birdY.value + birdYVelocity.value * dt / 1000
     birdYVelocity.value = birdYVelocity.value + GRAVITY * dt / 1000 // The gravity has been taken into account
     //console.log('Velocity:',birdYVelocity.value)
   })
 
-//Restart the game
-const restartGame = () => {
-  'worklet'; 
-  birdY.value = height / 3
-  birdYVelocity.value = 0
-  gameOver.value = false
-  x.value = width
-  runOnJS(moveTheMap)()
-  runOnJS(setScore)(0)
-  
+  //Restart the game
+  const restartGame = () => {
+    'worklet';
+    birdY.value = height / 3
+    birdYVelocity.value = 0
+    gameOver.value = false
+    x.value = width
+    runOnJS(moveTheMap)()
+    runOnJS(setScore)(0)
 
-}
+
+  }
 
 
   //This is for the tap gesture. So when we tap the bird will jump
   const gesture = Gesture.Tap().onStart(() => {
-    if(gameOver.value){
+    if (gameOver.value) {
       //restart
       restartGame()
     }
@@ -144,31 +187,29 @@ const restartGame = () => {
   })
 
   //This is for the rotation of the bird
-  const birdTransform = useDerivedValue(() =>{
+  const birdTransform = useDerivedValue(() => {
     return [
       {
-    rotate: interpolate(
-      birdYVelocity.value,
-      [-500, 500], // This is the range of the birdYVelocity
-      [-0.5, 0.5], // This is how much the bird will rotate
-      Extrapolation.CLAMP // This is to make sure the rotation is clamped between -0.5 and 0.5, not over that
-      ) 
-    }
+        rotate: interpolate(
+          birdYVelocity.value,
+          [-500, 500], // This is the range of the birdYVelocity
+          [-0.5, 0.5], // This is how much the bird will rotate
+          Extrapolation.CLAMP // This is to make sure the rotation is clamped between -0.5 and 0.5, not over that
+        )
+      }
     ]
   })
 
-//This is for the origin of the bird
+  //This is for the origin of the bird
   const birdOrigin = useDerivedValue(() => {
-    return { x: width / 4 + 32, y: birdY.value + 24}
+    return { x: width / 4 + 32, y: birdY.value + 24 }
   })
 
 
 
-  //Let's set the pipe offset. If offset is -100 pipe is upper and otherwise. Toppipe: offset - x, bottonpipe x + offset. 
-  //Thats cause we dont want to move the pipes same direction. 
-  const pipeOffset = 0
 
-  const fontFamily = Platform.select({ios: 'Helvetica',default: 'serif'})
+
+  const fontFamily = Platform.select({ ios: 'Helvetica', default: 'serif' })
   const fontStyle = {
     fontFamily,
     fontSize: 40,
@@ -179,64 +220,60 @@ const restartGame = () => {
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <GestureDetector gesture = {gesture}>
-    <Canvas 
-    style={{ width, height }}
-    >
-      {/* Background */}
-      <Image image={bg} width={width} height={height} fit={'cover'} />
-      {/* Pipes */}
-      <Image
-        image={pipeTop}
-        y={pipeOffset - 320}
-        x={x}
-        width={104}
-        height={640}
-      />
-      <Image
-        image={pipeBottom}
-        y={height - 320 + pipeOffset}
-        x={x}
-        width={104}
-        height={640}
-      />
-      {/* Base  */}
-      <Image
-        image={base}
-        width={width}
-        height={150}
-        y={height - 150}
-        x={0}
-        fit={'cover'}
-      />
-      <Group 
-      transform={birdTransform} origin={birdOrigin}
-      >
-      {/* Bird */}
-      <Image
+      <GestureDetector gesture={gesture}>
+        <Canvas
+          style={{ width, height }}
+        >
+          {/* Background */}
+          <Image image={bg} width={width} height={height} fit={'cover'} />
+          {/* Pipes */}
+          <Image
+            image={pipeTop}
+            y={pipeOffset - 320}
+            x={x}
+            width={pipeWidth}
+            height={pipeHeight}
+          />
+          <Image
+            image={pipeBottom}
+            y={height - 320 + pipeOffset}
+            x={x}
+            width={pipeWidth}
+            height={pipeHeight}
+          />
+          {/* Base  */}
+          <Image
+            image={base}
+            width={width}
+            height={150}
+            y={height - 150}
+            x={0}
+            fit={'cover'}
+          />
+          <Group
+            transform={birdTransform} origin={birdOrigin}>
+            {/* Bird */}
+            <Image
         image={bird}
         x={birdPos.x}
         y={birdY}
         width={64}
         height={48}
-      />
-     
-       </Group>
-       {/* Simulation*/}
-       <Circle cx={birdCenterX} cy={birdCenterY} r={25} width={64} height={48} color={'red'} />
-       
-       {/* Score */}
-        <Text
-        x={width / 2 -30}
-        y={100}
-        text= {score.toString()}
-        font={font}
-        
-        />
+  />
 
-       
-    </Canvas>
-    </GestureDetector>
+          </Group>
+          {/* Score */}
+          <Text
+            x={width / 2 - 30}
+            y={100}
+            text={score.toString()}
+            font={font}
+
+          />
+
+
+        </Canvas>
+      </GestureDetector>
     </GestureHandlerRootView>
   );
 };
