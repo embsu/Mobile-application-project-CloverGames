4f77bf18ae8823b9388d837aca80959f65ad00e2
Topi Ilmasti <122445154+SUPPlS@users.noreply.github.com>
2024-04-09 00:34:36 +0300

    memorygame
    kahdenkortin valinta bugi korjattu.

--- games/memorygame/components/MemoryGame.js
+++ games/memorygame/components/MemoryGame.js
@@ -27,6 +27,7 @@ const MemoryGame = () => {
   const [matchedCards, setMatchedCards] = useState([]); // Lisätty matchedCards-tilamuuttuja
   const [attempts, setAttempts] = useState(0); // Yritysten määrä
   const [points, setPoints] = useState(0); // Pisteiden määrä
+  const [gameStarted, setGameStarted] = useState(false); // Pelin aloitus
 
   const shuffleCards = (array) => {
     return array.sort(() => Math.random() - 0.5);
@@ -36,20 +37,42 @@ const MemoryGame = () => {
     setCards(shuffleCards(initialCards));
   }, []);
 
+  // useEffect(() => {
+    
+  //   if (gameStarted) {
+  //     console.log(isFlipped);
+  //     // Aseta kortit käännettyinä 2 sekunnin kuluttua pelin alkaessa
+  //     const timer = setTimeout(() => {
+  //       setCards(cards.map(card => ({ ...card, isFlipped: true })));
+  //       console.log(isFlipped);
+  //     }, 2000);
+  
+  //     // Palauta tyhjennystoiminto componentWillUnmount-metodilla
+  //     return () => clearTimeout(timer);
+  //   }
+  // }, [gameStarted, cards, isFlipped]);
+  
+
   const handleCardPress = (cardId) => {
+    //console.log(selectedCards)
     // Jos kortti on jo valittu tai kortti muodostaa jo parin, älä tee mitään
     if (selectedCards.includes(cardId) || matchedCards.includes(cardId)) {
       return;
     }
   
+    // Jos valittuja kortteja on kaksi, estä uusien korttien valinta
+    if (selectedCards.length === 2) {
+      return;
+    }
+    
     // Valitse kortti ja päivitä valittujen korttien tila
     setSelectedCards([...selectedCards, cardId]);
-  
+    
     // Jos valittuja kortteja on kaksi, tarkista ovatko ne pari
     if (selectedCards.length === 1) {
       const firstCard = cards.find((card) => card.id === selectedCards[0]);
       const secondCard = cards.find((card) => card.id === cardId);
-  
+    
       // Jos valitut kortit muodostavat parin, tyhjennä valitut kortit
       if (firstCard && secondCard && firstCard.content === secondCard.content) {
         setMatchedCards([...matchedCards, firstCard.id, secondCard.id]);
@@ -66,13 +89,46 @@ const MemoryGame = () => {
         setAttempts(attempts + 1)
         console.log('yritysten määrä:', attempts); // Päivitetään yritysten määrä
       }
-    } else if (selectedCards.length === 2) {
-      // Jos valittuja kortteja on jo kaksi, käännä ne takaisin 1,5 sekunnin kuluttua
-      setTimeout(() => {
-        setSelectedCards([]);
-      }, 1500);
     }
   };
+  
+  // const handleCardPress = (cardId) => {
+  //   // Jos kortti on jo valittu tai kortti muodostaa jo parin, älä tee mitään
+  //   if (selectedCards.includes(cardId) || matchedCards.includes(cardId)) {
+  //     return;
+  //   }
+  
+  //   // Valitse kortti ja päivitä valittujen korttien tila
+  //   setSelectedCards([...selectedCards, cardId]);
+  
+  //   // Jos valittuja kortteja on kaksi, tarkista ovatko ne pari
+  //   if (selectedCards.length === 1) {
+  //     const firstCard = cards.find((card) => card.id === selectedCards[0]);
+  //     const secondCard = cards.find((card) => card.id === cardId);
+  
+  //     // Jos valitut kortit muodostavat parin, tyhjennä valitut kortit
+  //     if (firstCard && secondCard && firstCard.content === secondCard.content) {
+  //       setMatchedCards([...matchedCards, firstCard.id, secondCard.id]);
+  //       setSelectedCards([])
+  //       setAttempts(attempts + 1); // Päivitetään yritysten määrä
+  //       console.log('yritysten määrä:', attempts); // Päivitetään yritysten määrä
+  //       setPoints(points + 1); // Päivitetään pisteiden määrä
+  //       console.log('pisteiden määrä:', points);
+  //     } else {
+  //       // Jos valitut kortit eivät muodosta paria, käännä kortit takaisin 1,5 sekunnin kuluttua
+  //       setTimeout(() => {
+  //         setSelectedCards([]);
+  //       }, 1500);
+  //       setAttempts(attempts + 1)
+  //       console.log('yritysten määrä:', attempts); // Päivitetään yritysten määrä
+  //     }
+  //   } else if (selectedCards.length === 2) {
+  //     // Jos valittuja kortteja on jo kaksi, käännä ne takaisin 1,5 sekunnin kuluttua
+  //     setTimeout(() => {
+  //       setSelectedCards([]);
+  //     }, 1500);
+  //   }
+  // };
 
   useEffect(() => {
     // Tarkista, onko valittuna kaksi korttia
@@ -105,6 +161,7 @@ const MemoryGame = () => {
       color="darkgray"
       isFlipped={selectedCards.includes(item.id) || matchedCards.includes(item.id)}
     />
+    
   );
 
   return (
