59ef408125446d083a829349edc1c86a69d54fc5
embsu <emilia.kumpuniemi@gmail.com>
2024-04-18 14:55:14 +0300

    variables to .env

--- firebase/Config.js
+++ firebase/Config.js
@@ -12,12 +12,12 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBaWDjP_Zcc1f6ppbDSseeTFCIKfrKfvbY",
-  authDomain: "clovergames-6443d.firebaseapp.com",
-  projectId: "clovergames-6443d",
-  storageBucket: "clovergames-6443d.appspot.com",
-  messagingSenderId: "274128948644",
-  appId: "1:274128948644:web:e0b2ca67eed6782c680531"
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID,
 };
 
 // Initialize Firebase
